0271e215388a12386400559a70fc0069bf90f173
Sukhada Gholba <sukhadagholba@gmail.com>
2019-05-01 16:49:18 -0700

    Updated mongoClient connection setup using the latest clinet object instead of the database object

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const morgan = require('morgan');
 const mongodb = require('mongodb');
+const assert = require('assert'); 
 const port = process.env.PORT || 5005;
-const server = express();
 const MongoClient = require('mongodb').MongoClient;
-let db;
+const notesApi = require('./Routes/notes/notes');
+
+let app = express();
+
 let uri;
+
 
 if(process.env.ENVIRONMENT==='production'){
 	uri = process.env.MONGODB_URI;
 }else{
+	require('dotenv').config();
 	uri = 'mongodb://localhost:27017';
 }
 
 
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
 
-server.get('/', (req,res)=>{
+app.use('/api/notes', notesApi);
+
+
+app.get('/', (req,res)=>{
 	res.send('Welcome to jotti ...');
 });
 
 
+
+
 // Initialize connection once
 
-MongoClient.connect(uri,{ useNewUrlParser: true }, (err, database)=> {
-  if(err) throw err;
+MongoClient.connect(uri,{ useNewUrlParser: true }, (err, client)=> {
 
-  db = database;
+  assert.equal(null, err);
+  console.log("Successfully connected to database\n");
+
+  app.locals.db = client.db(process.env.DB_NAME);
 
   // Start the application after the database connection is ready
 	
-  server.listen(port, ()=>{
+  app.listen(port, ()=>{
 	  console.log(`===Listening on port ${port}===`);
   });
 
